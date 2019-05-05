4c4b8ba9336542dd6f4ea86a6d976d2285791dda
basyura <basyura@gmail.com>
2019-05-06 00:19:06 +0900

    authenticate

--- TweetWPF/Actions/TweetWPF/Initialize.cs
+++ TweetWPF/Actions/TweetWPF/Initialize.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using Eleve;
+using Tweetinvi;
+using Tweetinvi.Models;
 
 namespace TweetWPF.Actions.TweetWPF
 {
@@ -10,6 +11,21 @@ namespace TweetWPF.Actions.TweetWPF
     {
         public override Task<ActionResult> Execute(object sender, EventArgs args, object parameter)
         {
+            string tokenPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config", "tweetwpf", "token.txt");
+            string[] lines = File.ReadAllLines(tokenPath);
+
+            Auth.SetUserCredentials(lines[0], lines[1], lines[2], lines[3]);
+
+            IAuthenticatedUser user = User.GetAuthenticatedUser();
+            if (user == null)
+            {
+                ViewModel.Message = "failed to authenticate.";
+            }
+            else
+            {
+                ViewModel.Message = user.ScreenName;
+            }
+
             return SuccessTask;
         }
     }
