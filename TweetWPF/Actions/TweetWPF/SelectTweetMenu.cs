7e623347f0d9409a230ca89877851c848279feb7
basyura <basyura@gmail.com>
2019-05-09 08:54:26 +0900

    added contextmenu

---
+++ TweetWPF/Actions/TweetWPF/SelectTweetMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Threading.Tasks;
+using Eleve;
+using Tweetinvi.Models;
+
+namespace TweetWPF.Actions.TweetWPF
+{
+    public class SelectTweetMenu : TweetWPFActionBase
+    {
+        public override Task<ActionResult> Execute(object sender, EventArgs args, object obj)
+        {
+            string key = obj as string;
+            if (string.IsNullOrEmpty(key))
+            {
+                return SuccessTask;
+            }
+
+            ITweet tweet = ViewModel.SelectedTweet;
+            if (tweet == null)
+            {
+                return SuccessTask;
+            }
+
+            return SuccessTask;
+        }
+    }
+}
